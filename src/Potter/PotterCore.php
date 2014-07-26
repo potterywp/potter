4abb0cd978ed312148e761935d63448190c0924d
Vinicius <luiz.vinicius73@gmail.com>
2014-07-25 22:53:45 -0300

    fix super-cpt vars

--- src/Potter/PotterCore.php
+++ src/Potter/PotterCore.php
@@ -6,8 +6,8 @@ class PotterCore
 {
     public function __construct()
     {
-        $SCPT_PLUGIN_URL = get_template_directory_uri().'/potter/vendor/potterywp/super-cpt/';
-        $SCPT_PLUGIN_DIR = get_template_directory().'/potter/vendor/potterywp/super-cpt';
+        $SCPT_PLUGIN_URL = get_template_directory_uri().'/vendor/potterywp/super-cpt/';
+        $SCPT_PLUGIN_DIR = get_template_directory().'/vendor/potterywp/super-cpt';
         Super_CPT_Loader::load($SCPT_PLUGIN_URL,$SCPT_PLUGIN_DIR);
     }
 }
