d15c3a1847f9316604012bae13528d87588ad300
Vinicius <luiz.vinicius73@gmail.com>
2014-11-13 13:52:44 -0200

    Bugfix

--- src/Potter/PotterCore.php
+++ src/Potter/PotterCore.php
@@ -46,10 +46,6 @@ class PotterCore
     {
         $this->themeDIR = THEME_DIR;
         $this->themeURL = THEME_URL;
-        $SCPT_PLUGIN_URL = $this->themeURL . 'vendor/potterywp/super-cpt/';
-        $SCPT_PLUGIN_DIR = $this->themeDIR . 'vendor/potterywp/super-cpt';
-
-        Super_CPT_Loader::load($SCPT_PLUGIN_URL, $SCPT_PLUGIN_DIR);
 
         $this->models = new Collection();
         $this->widgets = new Collection();
